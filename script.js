2fa2abc3ddfd7d7ac3ef278d75e350b7061d3633
STIVENSON.DEV <96761629+StivensonSS@users.noreply.github.com>
2022-07-03 22:57:49 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function obtenFibo (num) {
+    let Array = [1, 1]
+    for (let i = 2; i < num; i++) {
+       Array.push(Array[i - 1] + Array[i - 2])
+    }
+    return Array
+}
+
+console.log(obtenFibo(12))
